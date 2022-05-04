64c78d2897ff10285c014daae81fe51de1092b2f
Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>
2022-05-04 11:23:02 +0100

    Use blade files
    Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

--- views/dashboard/index.blade.php
+++ views/dashboard/index.blade.php
@@ -2,13 +2,13 @@
     <p class="text-right">
         <a
             target="_blank"
-            href="<?= url('horizon') ?>">
+            href="{{ url('horizon') }}">
             Standalone page <i class="fa fa-external-link"></i>
         </a>
     </p>
 </div>
 
-<iframe id="horizon-iframe" src="<?= url('horizon') ?>"></iframe>
+<iframe id="horizon-iframe" src="{{ url('horizon') }}"></iframe>
 
 <style>
     #horizon-iframe {
