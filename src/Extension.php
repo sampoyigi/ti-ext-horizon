tastyigniter/ti-ext-translate

Refactor code to use parameter, return type hints
Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

src/Extension.php
@@ -25,7 +25,7 @@ class Extension extends BaseExtension
         resolve(EventRegistry::class)->bootTranslatableModels();
     }
 
-    public function registerFormWidgets()
+    public function registerFormWidgets(): array
     {
         return [
             \Igniter\Translate\FormWidgets\TRLText::class => ['code' => 'trltext'],

sampoyigi/ti-ext-horizon

src/Extension.php
@@ -47,12 +47,12 @@ class Extension extends BaseExtension
         });
     }
 
-    public function registerSchedule($schedule)
+    public function registerSchedule(string $schedule)
     {
         $schedule->command('horizon:snapshot')->everyFiveMinutes();
     }
 
-    public function registerPermissions()
+    public function registerPermissions(): array
     {
         return [
             'SamPoyigi.Horizon.Access' => [
@@ -62,7 +62,7 @@ class Extension extends BaseExtension
         ];
     }
 
-    public function registerNavigation()
+    public function registerNavigation(): array
     {
         return [
             'tools' => [