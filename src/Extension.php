tastyigniter/ti-ext-automation

Regroup permissions
Signed-off-by: Sam Poyigi <6567634+sampoyigi@users.noreply.github.com>

src/Extension.php
@@ -31,7 +31,7 @@ class Extension extends BaseExtension
         return [
             'Igniter.Automation.Manage' => [
                 'description' => 'Create, modify and delete automations',
-                'group' => 'module',
+                'group' => 'admin',
             ],
         ];
     }

sampoyigi/ti-ext-horizon

src/Extension.php
@@ -56,7 +56,7 @@ class Extension extends BaseExtension
     {
         return [
             'SamPoyigi.Horizon.Access' => [
-                'group' => 'module',
+                'group' => 'advanced',
                 'description' => 'Access to the Horizon dashboard',
             ],
         ];